cc415bfa4c4da1a9df783c9db348214a47968a4f
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2026-01-28 21:34:05 -0300

    add class exercises

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -193,3 +193,214 @@ func permitirAcesso(senha: String, hora: Int) -> String {
 }
 
 print(permitirAcesso(senha: "1234", hora: 7))
+
+// Desafio 1 – Sistema de Pagamento
+// 📌 Descrição
+// Você está criando um sistema que processa diferentes formas de pagamento.
+// Cada tipo de pagamento calcula a taxa de forma diferente.
+// O que precisa ser feito
+// Crie uma classe base chamada Pagamento
+// Essa classe deve ter um método calcularValorFinal(valor: Double) -> Double
+// Crie duas subclasses:
+// CartaoCredito
+// Pix
+// Cada forma de pagamento deve calcular a taxa de forma diferente:
+// Cartão de crédito: taxa de 5%
+// Pix: sem taxa
+// Crie um array do tipo [Pagamento]
+// Percorra esse array e imprima o valor final de cada pagamento
+
+// Regras
+// Use override
+// Não use if ou switch para decidir o tipo de pagamento
+
+class Pagamento {
+    var paymentValue: Double
+
+    init(paymentValue: Double) {
+        self.paymentValue = paymentValue
+    }
+
+    func calcularValorFinal() -> Double {
+        return paymentValue
+    }
+}
+
+class CartaoCredito: Pagamento {
+    override func calcularValorFinal() -> Double {
+        return paymentValue * 0.95
+    }
+}
+
+class Pix: Pagamento {
+    override func calcularValorFinal() -> Double {
+        return paymentValue
+    }
+}
+
+let pagamentos: [Pagamento] = [CartaoCredito(paymentValue: 100), Pix(paymentValue: 50), CartaoCredito(paymentValue: 34)]
+
+for pagamento in pagamentos {
+    print(pagamento.calcularValorFinal())
+}
+
+// Desafio 2 – Notificações do App
+// Descrição
+// Um app envia notificações por diferentes canais.
+// Cada canal envia a mensagem de uma forma diferente.
+// O que precisa ser feito
+
+// Crie uma classe base Notificacao
+// Crie o método enviar(mensagem: String)
+// Crie pelo menos duas subclasses:
+// Email
+// SMS
+// Cada classe deve sobrescrever o método enviar
+// Armazene as notificações em um array [Notificacao]
+// Envie a mesma mensagem para todas
+
+// Regras
+// O código que envia a mensagem não pode saber qual é o tipo da notificação
+
+class Notificacao {
+    func enviar(mensagem: String) {
+        print(mensagem)
+    }
+}
+
+class Email: Notificacao {
+    override func enviar(mensagem: String) {
+        print("Enviando email: \(mensagem)")
+    }
+}
+
+class Sms: Notificacao {
+    override func enviar(mensagem: String) {
+        print("Enviando SMS: \(mensagem)")
+    }
+}
+
+let notificacoes: [Notificacao] = [Email(), Sms(), Notificacao()]
+
+for notificacao in notificacoes {
+    notificacao.enviar(mensagem: "Breaking bad é bom demais")
+}
+
+// Desafio 3 – Cofre Digital
+
+// Descrição
+// Você precisa criar um cofre digital que protege um valor armazenado.
+// 🎯 O que precisa ser feito
+// Crie uma classe Cofre
+// O valor armazenado deve ser private
+// Crie métodos para:
+// Depositar valor
+// Sacar valor
+// O saque só pode acontecer se houver saldo suficiente
+// Crie um método para consultar o saldo
+
+// ⚠️ Regras
+// O saldo não pode ser acessado diretamente
+// Todo acesso deve passar pelos métodos da classe
+
+class Cofre {
+    private var safeValue: Double
+
+    init(value: Double) {
+        safeValue = value
+    }
+
+    func depositar(value: Double) {
+        safeValue += value
+    }
+
+    func sacar(value: Double) {
+        if value > safeValue {
+            print("Saldo insuficiente")
+        } else {
+            safeValue -= value
+            print("Sucesso! seu saldo agora é de: \(safeValue)")
+        }
+    }
+
+    func consulta() {
+        print("Seu saldo é de: \(safeValue)")
+    }
+}
+
+// Desafio 4 – Usuário Seguro
+// Descrição
+// Você está criando um sistema de usuários onde a senha precisa ser protegida.
+// O que precisa ser feito
+// Crie uma classe Usuario
+// A senha deve ser private
+// A senha só pode ser alterada por um método
+// A nova senha deve ter pelo menos 6 caracteres
+// Se não for válida, a senha não deve ser alterada
+
+// Regras
+// Não permita acesso direto à senha
+// A validação deve acontecer dentro da classe
+
+class Usuario {
+    private var senha: String
+
+    init(senha: String) {
+        self.senha = senha
+    }
+
+    func mudarSenha(novaSenha: String) {
+        if novaSenha.count > 6 {
+            senha = novaSenha
+        } else {
+            print("Senha invalida")
+        }
+    }
+}
+
+// Desafio 5 – Funcionários da Empresa
+// Descrição
+// Uma empresa possui funcionários de tipos diferentes, e cada um tem uma forma distinta de cálculo salarial.
+// O que precisa ser feito
+// Crie uma classe base Funcionario
+// Crie um método calcularSalario() -> Double
+// Crie subclasses como:
+// CLT
+// PJ
+// Cada tipo deve calcular o salário de forma diferente
+// Armazene os funcionários em um array [Funcionario]
+// Percorra o array imprimindo o salário de cada funcionário
+// Regras
+// Use polimorfismo
+// Não use if ou switch
+// O salário não pode ser um valor público e alterável diretamente
+
+class Funcionario {
+    var salario: Double
+
+    init(salario: Double) {
+        self.salario = salario
+    }
+
+    func calcularSalario() -> Double {
+        return salario
+    }
+}
+
+class CLT: Funcionario {
+    override func calcularSalario() -> Double {
+        return self.salario * 0.70
+    }
+}
+
+class PJ: Funcionario {
+    override func calcularSalario() -> Double {
+        return self.salario * 0.8
+    }
+}
+
+var functionarios: [Funcionario] = [CLT(salario: 3000.0), PJ(salario: 5000)]
+
+for functionario in functionarios {
+    print(functionario.calcularSalario())
+}
